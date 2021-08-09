d91b1e0dfafe6a2028c17993987121587f767752
ms747 <mayur.shah.ha@gmail.com>
2021-08-10 01:38:20 +0530

    Working JIT

---
+++ src/codegen.rs
@@ -0,0 +1,80 @@
+// Get address of native functions
+macro_rules! libc_func {
+    ($address:expr, $t:ty) => {
+        std::mem::transmute::<*const usize, $t>($address as *const usize)
+    };
+}
+
+pub struct Codegen {
+    pub code: String,
+    print_fn: extern "C" fn(u8) -> (),
+    pc: &'static str,
+    memory: &'static str,
+}
+
+// Jit calling convention
+// r14 - program counter
+// r15 - memory
+
+impl Codegen {
+    pub fn new() -> Self {
+        Self {
+            code: "[bits 64]\n".into(),
+            print_fn: unsafe { libc_func!(libc::putchar, extern "C" fn(u8) -> ()) },
+            pc: "r14",
+            memory: "r15",
+        }
+    }
+
+    pub fn setup(&mut self) {
+        self.code += &format!("setup:\n");
+        self.code += &format!("push {pc}\n", pc = self.pc);
+        self.code += &format!("push {memory}\n", memory = self.memory);
+        self.code += &format!("xor {pc}, {pc}\n", pc = self.pc);
+        self.code += &format!("mov {memory}, rdi\n", memory = self.memory);
+    }
+
+    pub fn cleanup(&mut self) {
+        self.code += &format!("cleanup:\n");
+        self.code += &format!("pop {memory}\n", memory = self.memory);
+        self.code += &format!("pop {pc}\n", pc = self.pc);
+        self.code += &format!("xor rax, rax\n");
+        self.code += &format!("ret");
+    }
+
+    pub fn next(&mut self, value: usize) {
+        self.code += &format!("add {pc}, {value}\n", pc = self.pc, value = value);
+    }
+
+    pub fn prev(&mut self, value: usize) {
+        self.code += &format!("sub {pc}, {value}\n", pc = self.pc, value = value);
+    }
+
+    pub fn inc(&mut self, value: usize) {
+        self.code += &format!(
+            "add byte [{memory} + {pc}], {value}\n",
+            memory = self.memory,
+            pc = self.pc,
+            value = value
+        );
+    }
+
+    pub fn dec(&mut self, value: usize) {
+        self.code += &format!(
+            "sub byte [{memory} + {pc}], {value}\n",
+            memory = self.memory,
+            pc = self.pc,
+            value = value
+        );
+    }
+
+    pub fn print(&mut self) {
+        self.code += &format!(
+            "movzx edi, byte [{memory} + {pc}]\n",
+            memory = self.memory,
+            pc = self.pc
+        );
+        self.code += &format!("mov rax, {:?}\n", self.print_fn);
+        self.code += &format!("call rax\n");
+    }
+}
---
+++ src/main.rs
@@ -0,0 +1,87 @@
+#![feature(rustc_private)]
+use std::process::Command;
+
+extern crate libc;
+
+mod codegen;
+
+use crate::codegen::Codegen;
+
+fn alloc_rwx(size: usize) -> &'static mut [u8] {
+    extern "C" {
+        fn mmap(
+            addr: *mut u8,
+            length: usize,
+            prot: i32,
+            flags: i32,
+            fd: i32,
+            offset: usize,
+        ) -> *mut u8;
+    }
+
+    unsafe {
+        let ret = mmap(0 as *mut u8, size, 7, 34, -1, 0);
+        assert!(!ret.is_null());
+        std::slice::from_raw_parts_mut(ret, size)
+    }
+}
+
+fn main() {
+    // BF memory
+    let mut memory = [0u8; 512];
+
+    let mut codegen = Codegen::new();
+
+    codegen.setup();
+    codegen.inc(72);
+    codegen.print();
+    codegen.dec(3);
+    codegen.print();
+    codegen.inc(7);
+    codegen.print();
+    codegen.print();
+    codegen.inc(3);
+    codegen.print();
+    codegen.dec(35);
+    codegen.print();
+    codegen.dec(12);
+    codegen.print();
+    codegen.inc(55);
+    codegen.print();
+    codegen.dec(8);
+    codegen.print();
+    codegen.inc(3);
+    codegen.print();
+    codegen.dec(6);
+    codegen.print();
+    codegen.dec(8);
+    codegen.print();
+    codegen.dec(58);
+    codegen.print();
+    codegen.cleanup();
+
+    std::fs::write("./test.s", codegen.code).unwrap();
+
+    let res = Command::new("nasm")
+        .args(&["./test.s", "-f", "bin", "-o", "test"])
+        .status()
+        .expect("Failed to run nasm.");
+
+    assert!(res.success(), "Failed to generate assembly");
+
+    let bytecode = std::fs::read("./test").expect("Failed to read file");
+
+    let jit_mem = alloc_rwx(16 * 1024 * 1024);
+
+    unsafe {
+        libc::memcpy(
+            jit_mem.as_mut_ptr() as *mut libc::c_void,
+            bytecode.as_ptr() as *const libc::c_void,
+            bytecode.len(),
+        );
+    }
+
+    let jitfn: fn(memory: &mut [u8; 512]) -> () = unsafe { std::mem::transmute(jit_mem.as_ptr()) };
+
+    jitfn(&mut memory);
+}
